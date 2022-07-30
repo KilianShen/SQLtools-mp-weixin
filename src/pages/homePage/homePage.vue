16ceb5c92890b92f86f530f6d721ff2c87f42428
shenqingli <shenqingli@apexsofe.com.cn>
2022-07-30 09:46:00 +0800

    mine

--- src/pages/homePage/homePage.vue
+++ src/pages/homePage/homePage.vue
@@ -1,10 +1,16 @@
 <template>
   <view class="grid">
-    <uni-grid :column="3" :showBorder="false" :square="false">
+    <uni-grid
+      :column="3"
+      :showBorder="false"
+      :square="false"
+      @change="goToPage"
+    >
       <uni-grid-item
         class="grid_item"
         v-for="(item, index) in list"
         :key="index"
+        :index="index"
       >
         <text class="text">{{ item.name }}</text>
         <uni-icons :type="item.type" size="40" :color="item.color"> </uni-icons>
@@ -13,41 +19,73 @@
   </view>
 </template>
   
-<script setup lang='ts'>
-import Mock from "mockjs";
+<script lang='ts'>
+import { defineComponent } from "@vue/runtime-core";
+export default defineComponent({
+  data() {
+    return {
+      list: [
+        {
+          name: "手持弹幕",
+          type: "chatbubble-filled",
+          color: "#5dd88d",
+        },
+        {
+          name: "尺子",
+          type: "settings",
+          color: "#5dd88d",
+        },
+        {
+          name: "图片水印",
+          type: "image-filled",
+          color: "#5dd88d",
+        },
+        {
+          name: "九格切图",
+          type: "images-filled",
+          color: "#5dd88d",
+        },
+        {
+          name: "二维码工具",
+          type: "scan",
+          color: "#5dd88d",
+        },
+        {
+          name: "地址信息查询",
+          type: "location",
+          color: "#5dd88d",
+        },
+        {
+          name: "testPage",
+          type: "more-filled",
+          color: "#5dd88d",
+          path: "/pages/testPage/testPage",
+        },
+      ],
+    };
+  },
 
-let list = [
-  {
-    name: "手持弹幕",
-    type: "chatbubble-filled",
-    color: "#5dd88d",
+  methods: {
+    goToPage(e: IObject) {
+      console.log("e", e.detail);
+      const { index } = e.detail;
+      const path = this.list[index].path;
+      if (!path) {
+        return;
+      }
+      wx.navigateTo({
+        url: `${path}`,
+        success: () => {
+          let pages = getCurrentPages();
+          console.info("navigateToSuccess! PageStack>>", getCurrentPages());
+        },
+        fail: (e: any) => {
+          console.error(e);
+        },
+      });
+    },
   },
-  {
-    name: "尺子",
-    type: "settings",
-    color: "#5dd88d",
-  },
-  {
-    name: "图片水印",
-    type: "image-filled",
-    color: "#5dd88d",
-  },
-  {
-    name: "九格切图",
-    type: "images-filled",
-    color: "#5dd88d",
-  },
-  {
-    name: "二维码工具",
-    type: "scan",
-    color: "#5dd88d",
-  },
-  {
-    name: "地址信息查询",
-    type: "location",
-    color: "#5dd88d",
-  },
-];
+});
 </script>
   
 <style scope lang='scss'>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,41 +1,81 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
+  <view>
+    <button
+      open-type="chooseAvatar"
+      @chooseavatar="onChooseAvatar"
+      class="chooseAvatar"
+    >
+      <image class="avatar" :src="avatar"></image>
+    </button>
+    <input
+      type="nickname"
+      class="weui-input"
+      placeholder="请输入昵称"
+      @confirm="confirm"
+    />
+    <text>{{ nickname }}</text>
   </view>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
+<script lang="ts">
+import { defineComponent, onBeforeMount, ref, reactive, toRefs } from "vue";
+
+export default defineComponent({
+  setup() {
+    let userInfo: IObject = reactive({
+      nickname: "xxx",
+      avatar:
+        "https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0",
+    });
+    onBeforeMount(() => {
+      try {
+        const wx_avatar = uni.getStorageSync("wx_avatar");
+        const wx_nickname = uni.getStorageSync("wx_nickname");
+
+        if (wx_avatar) {
+          userInfo.avatar = wx_avatar;
+        }
+        if (wx_nickname) {
+          userInfo.nickname = wx_nickname;
+        }
+      } catch (e) {
+        console.error("e", e);
+      }
+    });
+
+    const onChooseAvatar = (e: IObject) => {
+      const { avatarUrl } = e.detail;
+      userInfo.avatar = avatarUrl;
+      try {
+        uni.setStorageSync("wx_avatar", userInfo.avatar);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    const confirm = (e: IObject) => {
+      const { value = "" } = e.detail;
+      console.log("value", value);
+      userInfo.nickname = value;
+      try {
+        uni.setStorageSync("wx_nickname", userInfo.nickname);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    return { ...toRefs(userInfo), onChooseAvatar, confirm };
+  },
+});
 </script>
 
 <style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+.chooseAvatar {
+  height: 100rpx;
+  width: 100rpx;
+  padding: 0 !important;
 }
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+.avatar {
+  width: 100rpx;
+  height: 100rpx;
 }
 </style>
