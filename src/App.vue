e21e63d47b837a59a3bcaf04a09d022c7a5bc401
shenqingli <shenqingli@apexsofe.com.cn>
2022-08-08 16:17:18 +0800

    监听小程序更新

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,20 @@ onLaunch(() => {
       store.commit("systemInfo/SET_VALUE", res);
     },
   });
+
+  const updateManager = wx.getUpdateManager();
+  updateManager.onUpdateReady(function () {
+    wx.showModal({
+      title: "更新提示",
+      content: "新版本已经准备好，是否重启应用？",
+      success: (res: IObject) => {
+        if (res.confirm) {
+          // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+          updateManager.applyUpdate();
+        }
+      },
+    });
+  });
 });
 onShow(() => {
   console.log("App Show");
